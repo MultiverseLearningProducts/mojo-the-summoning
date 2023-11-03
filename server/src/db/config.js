23663be1e1eea1a0eaafa66fbcf131c39f07f3f5
mmaki12 <mmaki12@hotmail.com>
2023-11-03 11:01:46 +0000

    merge

--- server/src/db/config.js
+++ server/src/db/config.js
@@ -1,1 +1,11 @@
-// add your database connection here
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize({
+  dialect: 'sqlite', 
+  storage: 'your-database-file.sqlite', 
+});
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection to the database has been established successfully.');
+  })
